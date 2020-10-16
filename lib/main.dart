da9d5b90a3268ed9ef20055eb62622060e8cd683
Aryan <aryancraj@gmail.com>
2020-10-16 22:40:27 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:splashscreen/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Myapp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Myapp extends StatefulWidget {
+  @override
+  _MyappState createState() => _MyappState();
+}
+class _MyappState extends State<Myapp>{
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      children: <Widget>[
+        Image.asset('assets/blood.jpg',height: 100.0,),
+      ]
+      seconds: 10,
+      backgroundColor: Colors.red,
+      loaderColor: Colors.yellow,
+      photoSize: 150.0,
+      navigateAfterSeconds: mainscreen(),
+    );
+  }
+}
+
+class mainscreen extends StatefulWidget{
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<mainscreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text('Blood Donation'),
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Donor Login',
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '"Share a little, Care a little"',
+                      style: TextStyle(fontSize: 20),
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    //forgot password screen
+                  },
+                  textColor: Colors.green,
+                  child: Text('Forgot Password'),
+                ),
+                Container(
+                    height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.green,
+                      child: Text('Login'),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(passwordController.text);
+                      },
+                    )),
+                Container(
+                    child: Row(
+                      children: <Widget>[
+                        Text('Be a donor?'),
+                        FlatButton(
+                          textColor: Colors.green,
+                          child: Text(
+                            'Register Here',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {
+                            //signup screen
+                          },
+                        )
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ))
+              ],
+            )));
+  }
+}
