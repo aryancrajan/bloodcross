d88d063ad2744742addf3819f51af6644fa5340f
Aswathy Rajan <64630933+Aswathy21@users.noreply.github.com>
2020-11-06 12:10:23 +0530

    Registration form

--- lib/main.dart
+++ lib/main.dart
@@ -18,25 +18,189 @@ class _MyappState extends State<Myapp>{
       seconds: 5,
       backgroundColor: Colors.red,
       image: Image.asset('assets/blood.gif'),
-      loaderColor: Colors.yellow,
+      title: Text('"BLOODCROSS"', style: TextStyle(color:Colors.white, fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,fontSize:25)),
+      loaderColor: Colors.blueAccent,
       photoSize: 150.0,
       navigateAfterSeconds: mainscreen(),
     );
   }
 }
 class SecondRoute extends StatelessWidget {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController groupController = TextEditingController();
+  TextEditingController numberController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Register Here"),
+        backgroundColor: Colors.red ,
+        title: Text("REGISTER"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);// Navigate back to first route when tapped.
-          },
-          child: Text('Go back!'),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                padding: EdgeInsets.only(top: 15.0, left: 20.0, right: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                          labelText: 'NAME',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                          // hintText: 'EMAIL',
+                          // hintStyle: ,
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.redAccent))),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                          labelText: 'EMAIL ID',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                          // hintText: 'EMAIL',
+                          // hintStyle: ,
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.redAccent))),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                          labelText: 'PASSWORD',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                          // hintText: 'EMAIL',
+                          // hintStyle: ,
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.redAccent))),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextField(
+                      controller: ageController,
+                      decoration: InputDecoration(
+                          labelText: 'AGE',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                          // hintText: 'EMAIL',
+                          // hintStyle: ,
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.redAccent))),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextField(
+                      controller: groupController,
+                      decoration: InputDecoration(
+                          labelText: 'BLOOD GROUP',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.redAccent))),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 10.0),
+                    TextField(
+                      controller: numberController,
+                      decoration: InputDecoration(
+                          labelText: 'MOBILE NO ',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.redAccent))),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextField(
+                      controller: addressController,
+                      decoration: InputDecoration(
+                          labelText: 'LOCATION',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                          // hintText: 'EMAIL',
+                          // hintStyle: ,
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.redAccent))),
+                    ),
+                    SizedBox(height: 25.0),
+                    Container(
+                        height: 40.0,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(25.0),
+                          shadowColor: Colors.redAccent,
+                          color: Colors.red,
+                          elevation: 7.0,
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: Center(
+                              child: Text(
+                                'SUBMIT',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ),
+                        )),
+                    SizedBox(height: 20.0),
+                    Container(
+                      height: 40.0,
+                      color: Colors.transparent,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: Colors.red,
+                        elevation: 10.0,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child:
+
+                          Center(
+                            child: Text('BACK',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Montserrat',
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+            )
+          ],
         ),
       ),
     );
@@ -55,7 +219,7 @@ class _State extends State<mainscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.green,
+          backgroundColor: Colors.red,
           title: Text('Blood Donation'),
         ),
         body: Padding(
@@ -64,11 +228,11 @@ class _State extends State<mainscreen> {
               children: <Widget>[
                 Container(
                     alignment: Alignment.center,
-                    padding: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(30),
                     child: Text(
-                      'Donor Login',
+                      'User Login',
                       style: TextStyle(
-                          color: Colors.green,
+                          color: Colors.brown,
                           fontWeight: FontWeight.w500,
                           fontSize: 30),
                     )),
@@ -84,7 +248,7 @@ class _State extends State<mainscreen> {
                   child: TextField(
                     controller: nameController,
                     decoration: InputDecoration(
-                      border: OutlineInputBorder(),
+                      border: OutlineInputBorder(borderSide: BorderSide(color: Colors.redAccent)),
                       labelText: 'User Name',
                     ),
                   ),
@@ -95,7 +259,7 @@ class _State extends State<mainscreen> {
                     obscureText: true,
                     controller: passwordController,
                     decoration: InputDecoration(
-                      border: OutlineInputBorder(),
+                      border: OutlineInputBorder(borderSide: BorderSide(color: Colors.redAccent)),
                       labelText: 'Password',
                     ),
                   ),
@@ -104,7 +268,7 @@ class _State extends State<mainscreen> {
                   onPressed: () {
                     //forgot password screen
                   },
-                  textColor: Colors.green,
+                  textColor: Colors.brown,
                   child: Text('Forgot Password'),
                 ),
                 Container(
@@ -112,8 +276,8 @@ class _State extends State<mainscreen> {
                     padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
                     child: RaisedButton(
                       textColor: Colors.white,
-                      color: Colors.green,
-                      child: Text('Login'),
+                      color: Colors.redAccent,
+                      child: Text('LOGIN',style: TextStyle(fontSize: 15)),
                       onPressed: () {
                         print(nameController.text);
                         print(passwordController.text);
@@ -122,11 +286,11 @@ class _State extends State<mainscreen> {
                 Container(
                     child: Row(
                       children: <Widget>[
-                        Text('Be a donor?'),
+                        Text('New here?'),
                         FlatButton(
-                          textColor: Colors.green,
+                          textColor: Colors.brown,
                           child: Text(
-                            'Register Here',
+                            'Register',
                             style: TextStyle(fontSize: 20),
                           ),
                           onPressed: () {
